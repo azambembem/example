14e0bebb496c26ab3be1295990940408e62493ff
azambembem <azamjan980713@gmail.com>
2025-02-20 20:42:09 +0900

    portfolio beginner

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+let user;
+
+fs.readFile("db/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 kirish qismi
 app.use(express.static("public"));
@@ -19,6 +31,11 @@ app.post("/create-item", (req, res) => {
   console.log(req.body);
   res.json({ status: "success" });
 });
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
 app.get("/", (req, res) => {
   res.render("harid");
 });
